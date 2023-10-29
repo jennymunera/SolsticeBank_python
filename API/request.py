f922a3b99e24a562e20b623ff58d133baa742fb6
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2023-10-28 19:41:15 -0500

    config final request, load data and config dates

--- API/request.py
+++ API/request.py
@@ -2,82 +2,49 @@
 # this cell enables relative path imports
 import os
 from dotenv import load_dotenv
-load_dotenv()
-
-#%%
-_PROJECT_PATH: str = os.environ["_project_path"]
-_CSV_DATA_FILENAME: str = os.environ["_csv_data_filename"]
-SOLSTICE_BANK: str = os.environ["solstice_bank"]
-#%%
-import sys
-from pathlib import Path
-project_path = Path(_PROJECT_PATH)
-sys.path.append(str(project_path))
-
-# %%
-# import all your modules here
 import time
 import json
 import pandas as pd
 
-import API.config_v2 as cfg
 from API.library_ubidots_v2 import Ubidots as ubi
 
-# %%
-# set your constants here
-baseline=cfg.BASELINE
-study=cfg.STUDY
+SOLSTICE_BANK: str = os.environ["solstice_bank"]
+load_dotenv()
 
-# %%
-df_devices = ubi.get_available_devices_v2(label=SOLSTICE_BANK, level='group', page_size=1000)
+LST_VAR = [
+    "ilu-consumo-activa",
+    "aa-consumo-activa",
+    "front-consumo-activa"]
 
-#%%
-df_devices = df_devices[df_devices['device_label'].isin(['bc49','bc-291-las-palmas','bc37','bc38'])]
+LST_DVC = ['bc49','bc-291-las-palmas','bc37','bc38']
+    
+# time = ['2023-10-20','2023-10-21']
 
-df_vars = ubi.get_available_variables(list(df_devices['device_id']))
 
-# %%
-df_vars = df_vars[df_vars['variable_label'].isin(cfg.WHITELISTED_VAR_LABELS)]
-VAR_IDS_TO_REQUEST = list(df_vars['variable_id'])
-VAR_ID_TO_LABEL = dict(zip(df_vars['variable_id'], df_vars['variable_label']))
-
-# %%
-CHUNK_SIZE = 10
-DATE_INTERVAL_REQUEST = {'start': baseline[0], 'end': study[1]}
-df = None
-lst_responses = []
-n_vars = len(VAR_IDS_TO_REQUEST)
-print(f"Making request for the following interval: Baseline:{baseline}, Study:{study}")
-for idx in range(0, ubi.ceildiv(len(VAR_IDS_TO_REQUEST), CHUNK_SIZE)):
-    idx_start = idx * CHUNK_SIZE
-    idx_end = (idx + 1) * CHUNK_SIZE
-    chunk = VAR_IDS_TO_REQUEST[idx_start:idx_end]
+def request(Time, LST_VAR, LST_DVC):
+    df_devices = ubi.get_available_devices_v2(label=SOLSTICE_BANK, level='group', page_size=1000)
+    df_devices = df_devices[df_devices['device_label'].isin(LST_DVC)]
+    df_vars = ubi.get_available_variables(list(df_devices['device_id']))
+    df_vars = df_vars[df_vars['variable_label'].isin(LST_VAR)]
+    VAR_IDS_TO_REQUEST = list(df_vars['variable_id'])
+    VAR_ID_TO_LABEL = dict(zip(df_vars['variable_id'], df_vars['variable_label']))
+    DATE_INTERVAL_REQUEST = {'start': Time[0], 'end': Time[1]}
+    df = None
+    lst_responses = []
+    
     response = ubi.make_request(
-        chunk, 
-        DATE_INTERVAL_REQUEST, 
-    )
-    print("RESPUESTA : ",response.json())
-    if response.status_code == 204 or response.status_code >= 500:
-        print(f"Empty response for chunk {idx}")
-        time.sleep(10)
-        response = ubi.make_request(
-        chunk, 
-        DATE_INTERVAL_REQUEST,)
-    current_idx = idx_end+1
-    if (current_idx > n_vars):
-        current_idx = n_vars
-    print(f"Progress: {100*(current_idx)/n_vars:0.1f}%")
-    print(f"Response status code: {response.status_code}")
-    if (response.status_code != 204) and  (len(response.json()['results']) >0 ):
-        lst_responses.append(response)
-    else: 
-        print(f"Empty response for chunk {idx}")
-df = ubi.parse_response(lst_responses, VAR_ID_TO_LABEL)
+        VAR_IDS_TO_REQUEST, 
+        DATE_INTERVAL_REQUEST)
 
+    lst_responses.append(response)
+
+    df = ubi.parse_response(lst_responses, VAR_ID_TO_LABEL)
+
+    # df.to_csv('datos.csv')
+
+    return df
 
 # %%
-#len(response.json()['results'][0][0])
 
-
+# request(time, LST_VAR, LST_DVC)
 # %%
-#df.to_csv(f"{_CSV_DATA_FILENAME}", index=False)
--- Home.py
+++ Home.py
@@ -6,12 +6,22 @@ from numerize.numerize import numerize
 from datetime import date, timedelta, datetime
 from streamlit_extras.metric_cards import style_metric_cards
 import time 
-
-
-#from API.request import df
+from API.request import request
+from API.library_ubidots_v2 import Ubidots as ubi
 from reports.sabana import *
+import plotly.express as px
 
 
+load_data = False
+
+
+LST_VAR = [
+    "ilu-consumo-activa",
+    "aa-consumo-activa",
+    "front-consumo-activa"]
+
+LST_DVC = ['bc49','bc-291-las-palmas','bc37','bc38']
+    
 
 st.set_page_config(page_title="Solstice Dashboard",page_icon="📊",layout="wide")
 st.write("<div style='text-align: center;'><h1>🔋 Monitoreo de Energia Solstice Bank</h1></div>", unsafe_allow_html=True)
@@ -22,53 +32,114 @@ st.markdown("##")
 
 
 ## widget reloj
-def reloj():
-    widget_container_reloj = st.container()
-    clock_date_placeholder = widget_container_reloj.empty()
-    while True:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        current_date = now.strftime("%Y-%m-%d")
+# def reloj():
+#     widget_container_reloj = st.container()
+#     clock_date_placeholder = widget_container_reloj.empty()
 
-        clock_date_placeholder.markdown(
-            f'<div style="border: 2px solid #e0e0e0; border-radius: 10px; padding: 10px;width:15%;text-align: center;">'
-            f'<p>Hora y Fecha<p>'
-            f'<p>{current_time}</p>'
-            f'<p>{current_date}</p>'
-            f'</div>',
-            unsafe_allow_html=True
-        )
-        time.sleep(1)
+#     now = datetime.now()
+#     current_time = now.strftime("%H:%M:%S")
+#     current_date = now.strftime("%Y-%m-%d")
+#     clock_date_placeholder.markdown(
+#         f'<div style="border: 2px solid #e0e0e0; border-radius: 10px; padding: 10px;width:15%;text-align: center;">'
+#         f'<p>Hora y Fecha<p>'
+#         f'<p>{current_time}</p>'
+#         f'<p>{current_date}</p>'
+#         f'</div>',
+#         unsafe_allow_html=True
+#     )
 
+
+
+
+    
 
 #sidebar
 def sideBarOptions():
-    st.sidebar.image("img/logosolstice.png",use_column_width=True)
+    df = pd.read_csv('/Users/jennymunera/Documents/GitHub/SolsticeBank_python/API/datos.csv')
+    # df=[]
+
+    st.sidebar.image("/Users/jennymunera/Documents/GitHub/SolsticeBank_python/img/logosolstice.png",use_column_width=True)
     st.sidebar.markdown("##")
 
     ##select page
     page = st.sidebar.selectbox("Selecciona una Sede", ["Sabana", "Palmas", "Olimpica","Mayales"])
-    
+
     st.sidebar.markdown("##")
+    start_date=st.sidebar.date_input("Start Date",date.today()-timedelta(days=1))
+    end_date=st.sidebar.date_input(label="End Date")
+    rq_time = [start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')]
     st.sidebar.markdown("##")
-    st.sidebar.image("img/logonexus.png",use_column_width=True)
 
     if page == "Sabana":
         st.title("Sede Sabana de Torres")
-        reloj()
+        # reloj()
 
     elif page == "Palmas":
         st.title("Sede Las Palmas")
-        reloj()
+        # reloj()
 
     elif page == "Olimpica":
         st.title("Sede Olimpica")
-        reloj()
+        # reloj()
 
     elif page == "Mayales":
         st.title("Sede Mayales")
-        reloj()
+        # reloj()
 
-sideBarOptions()
+    load_data=st.sidebar.button('Cargar datos')
+
+    if load_data:
+        # df = request(rq_time,LST_VAR, LST_DVC)
+        load_data = False
+
+    st.sidebar.image("/Users/jennymunera/Documents/GitHub/SolsticeBank_python/img/logonexus.png",use_column_width=True)
+
+    return df, page, load_data
+
+def graficas(df, page):
+
+    if page == 'Sabana':
+
+        df_sabana = df[df['device']== 'bc49']
+
+
+        fig = px.bar(
+            df_sabana,
+            x="datetime",
+            y="value",
+            barmode='group',
+            color='variable',
+            labels={'month_day':'Mes - Día', 'value':'Consumo [kWh]'},
+            title=f"Consumo diario de energía activa [kWh]",
+        )
+
+        left,right,center=st.columns(3)
+        left.plotly_chart(fig,use_container_width=True)
+        right.plotly_chart(fig,use_container_width=True)
+    
+        with center:
+            st.plotly_chart(fig, use_container_width=True)
+    return
+
+
+
+
+
+df, page, load_data = sideBarOptions()
+
+print(len(df))
+
+if len(df) > 0:
+    graficas(df, page)
+    
+
+
+
+
+
+
+
+
+
 
 
